292d922ed5c69df8b2eeace2ef9cd98c3c0551c2
JossueE <110933714+JossueE@users.noreply.github.com>
2023-10-30 20:23:07 -0600

    Update apys.py

--- apys.py
+++ apys.py
@@ -1,6 +1,6 @@
 #Here put your OpenAI api_key
-API_KEY_OPEN_AI = "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"
+API_KEY_OPEN_AI = " "
 
 #Here put the client_secret and secret_id of spotify
-CLIENT_ID_SP = '7cb6de8385fe4bb4b7ef62542dbd9903'
-CLIENT_SECRET_SP = '7f7d5a7fe1e648618dcb5a94f96d09d6'
+CLIENT_ID_SP = ' '
+CLIENT_SECRET_SP = ' '
