6c542b2b4d53bd67d4fa5887b094e1c38e8d7c77
JossueE <110933714+JossueE@users.noreply.github.com>
2023-10-30 20:19:08 -0600

    Add files via upload

---
+++ apys.py
@@ -0,0 +1,6 @@
+#Here put your OpenAI api_key
+API_KEY_OPEN_AI = "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"
+
+#Here put the client_secret and secret_id of spotify
+CLIENT_ID_SP = '7cb6de8385fe4bb4b7ef62542dbd9903'
+CLIENT_SECRET_SP = '7f7d5a7fe1e648618dcb5a94f96d09d6'
